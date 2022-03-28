bbcbd3f8be15f44ff7b9f7e8662bc6c78a1ac259
lei hehl <lei.hehl@shopee.com>
2022-03-29 00:08:30 +0800

    tool(组件): feat

--- packages/themes/index.js
+++ packages/themes/index.js
@@ -1,6 +1,6 @@
-// export const a = 1;
-// export const b = 2;
-// export const c = 3;
-// export const d = 4;
-// export const d = 5;
-// console.log(1);
+export const a = 1;
+export const b = 2;
+export const c = 3;
+export const d = 4;
+export const d = 5;
+console.log(1);
