8a0efc2c3bc2ace1b3121d8cf6b87d2c89d10117
lei.he <18375721476@sina.cn>
2022-03-31 00:34:29 +0800

    tool(其它变更): fix: test

--- packages/components/src/components.d.ts
+++ packages/components/src/components.d.ts
@@ -21,6 +21,7 @@ export namespace Components {
         "middle": string;
     }
     interface StenButton {
+        "text": string;
     }
 }
 declare global {
@@ -57,6 +58,7 @@ declare namespace LocalJSX {
         "middle"?: string;
     }
     interface StenButton {
+        "text"?: string;
     }
     interface IntrinsicElements {
         "my-component": MyComponent;
--- packages/components/src/components/sten-button/sten-button.tsx
+++ packages/components/src/components/sten-button/sten-button.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, Host, h, Prop, State, Watch,Element } from '@stencil/core';
 
 @Component({
   tag: 'sten-button',
@@ -6,11 +6,35 @@ import { Component, Host, h } from '@stencil/core';
   shadow: true,
 })
 export class StenButton {
+  @Prop() text: string;
+  
+  @State() num: number = 0;
+
+  @Element() el: HTMLElement;
+
+  @Watch('num')
+  watchPropHandler(newValue: boolean, oldValue: boolean) {
+    console.log('The old value of num is: ', oldValue);
+    console.log('The new value of num is: ', newValue);
+  }
+
+  getListHeight(): number {
+    console.log(this.el);
+    return this.el.getBoundingClientRect().height;
+  }
 
   render() {
     return (
       <Host>
-        <button class="sten-button"><slot></slot></button>
+        <button 
+          class="sten-button"
+          onClick={() => {
+            this.num += 1;
+            this.getListHeight();
+          }}
+        >
+          {this.text}{this.num}<slot></slot>
+        </button>
       </Host>
     );
   }
